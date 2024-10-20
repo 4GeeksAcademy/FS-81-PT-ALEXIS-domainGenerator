15eefaaa1b4bccfece1782f60cc018c226a47edc
Cadivals <4geeksalex@gmail.com>
2024-10-20 15:33:26 +0000

    feast:comand

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "his"];
+  let adj = ["great", "big", "small", "kind"];
+  let noun = ["jogger", "racoon", "tiger", "man"];
+  let dom = [".com", ".org", ".es", ".io"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < dom.length; l++) {
+          console.log(pronoun[i] + adj[j] + noun[k] + dom[l]);
+        }
+      }
+    }
+  }
 };
